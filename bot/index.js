2a246d166008c977e941ff31338f69dcdd5cf156
de olagundoye <de.olagundoye@gmail.com>
2017-05-03 15:11:42 -0400

    render ejs

--- bot/index.js
+++ bot/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const config = require('./config');
-
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send('hello world');
+app.use(express.static(__dirname + '/views'));
+app.set ('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('hello world');
 });
+
+app.get('/test', (req, res) => {
+	res.render('test');
+})
 
 app.listen(config.port);
