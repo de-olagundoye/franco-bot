b5c8b0b52c6936afd43d9899a8122e528ad31310
Douglas Grogg <dgrogg@urbanoutfitters.com>
2017-05-04 12:52:32 -0400

    search test

--- bot/index.js
+++ bot/index.js
@@ -4,6 +4,7 @@ const wit = require('node-wit').Wit;
 const config = require('./config');
 const witToken = config.witai.serverAccessToken;
 const app = express();
+const _ = require('lodash');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,14 +17,22 @@ app.get('/bot/talk', (req, res) => {
 
 app.post('/bot/talk', (req, res) => {
     const text = req.body.text;
-    res.send('you sent "' + text + '".');
 
     const client = new wit({accessToken: witToken});
     client.message(text, {})
         .then((data) => {
-        console.log('wit.ai response: ' + JSON.stringify(data));
-    })
-    .catch(console.error);
+            res.send(search(data, res));
+        })
+        .catch(console.error);
 });
+
+function search(data, res) {
+    if(_.get(data, 'entities.intent', false)) {
+        return data;
+    }
+    else {
+        return res.render('color');
+    }
+}
 
 app.listen(config.port);
